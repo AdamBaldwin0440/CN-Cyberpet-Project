530e3bd18c2528bde94424cc10110cadbe76ad1b
Imperialii <devavictrix11@gmail.com>
2023-02-02 13:55:21 +0000

    Commented out some lines for testing purposes for now and added a table for tracking stats for easy reading

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const Feedback = document.getElementById("InteractionsFeedback");
-const NamePet = document.getElementById("NamePet");
-const NameSub = document.getElementById("NameSub");
-const TypeDrag = document.getElementById("TypeDrag");
-const TypeGob = document.getElementById("TypeGob");
-const NameHeader = document.getElementById("NameHeader");
+// *Commented out elements for JS testing sorry* - Lee
+// const Feedback = document.getElementById("InteractionsFeedback");
+// const NamePet = document.getElementById("NamePet");
+// const NameSub = document.getElementById("NameSub");
+// const TypeDrag = document.getElementById("TypeDrag");
+// const TypeGob = document.getElementById("TypeGob");
+// const NameHeader = document.getElementById("NameHeader");
 
 let PetName
 
 class Pet{
-    constructor(name, hunger, thirst, energy, happiness){
+    constructor(name){
         this.name = name;
         this.hunger = 100;
         this.thirst = 100;
@@ -22,23 +23,20 @@ class Dragon extends Pet{
     }
    Drinks(){
     this.thirst += 20;
-    console.log(`${this.name} is drinking`);
-    Feedback.innerText = `${this.name} is drinking`
+    // Feedback.innerText = `${this.name} is drinking`;
     //interval/timeout to reset it back to blank?
-    return this
+    return this;
    } 
-   Devour(){
+   Eats(){
     this.hunger += 20;
     this.thirst -= 10;
-    console.log(`${this.name} has devoured a princess/maiden/knight`);
-    Feedback.innerText = `${this.name} has devoured someone`;
-    return this
+    // Feedback.innerText = `${this.name} has devoured someone`;
+    return this;
    }
-   Sleep(){
+   Sleeps(){
     this.energy += 20;
     this.hunger -= 10;
-    console.log(`${this.name} is sleeping on their hoard of gold`)
-    Feedback.innerText = `${this.name} is sleeping on their hoard of gold`;    
+    // Feedback.innerText = `${this.name} is sleeping on their hoard of gold`;    
     return this;
    }
    BurnVillage(){
@@ -46,8 +44,25 @@ class Dragon extends Pet{
     this.hunger +10;
     this.energy -= 20;
     this.thirst -= 10;
-    console.log(`${this.name} is having fun rampaging across the land. How lovely`);
-    Feedback.innerText = `${this.name} is burning a village, bless them`;    
-    return this
+    // Feedback.innerText = `${this.name} is burning a village, bless them`;    
+    return this;
+   }
+   Stats() {
+    return console.table({
+        name: this.name,
+        hunger: this.hunger,
+        thirst: this.thirst,
+        energy: this.energy,
+        happiness: this.happiness,
+    });
    }
 }
+
+const testDragonName = new Dragon()
+
+testDragonName.Drinks()
+testDragonName.Eats()
+testDragonName.Sleeps()
+testDragonName.BurnVillage()
+testDragonName.Stats()
+
